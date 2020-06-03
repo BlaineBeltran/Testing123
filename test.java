1465b48cb8434eb8f7f0a9d7416de9f72aed6389
BlaineBeltran <55524257+ComputerEng21@users.noreply.github.com>
2020-06-02 21:06:39 -0500

    Added semicolons so code should run

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
-public class Testing {
+public class Test {
   public static void main(String Args[]) {
     System.out.printf("%nHello World");
     System.out.printf("%nGoodbye");
-    System.out.printf("%nI'm Back!")
+    System.out.printf("%nI'm Back!");
+    System.out.printf("%nHope!");
   }// END main()
 }// END APPLICATION class Testing
