0a819da6d7904c84de36d7aae33e388d6581c0af
BlaineBeltran <55524257+ComputerEng21@users.noreply.github.com>
2020-06-03 16:38:18 -0500

    Added scanner to take in user input

--- test.java
+++ test.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Test {
   public static void main(String Args[]) {
-    System.out.printf("%nHello World");
+
+    public String response;
+
+    System.out.printf("%nWhat is your name?  ");
+    response = input.nextLine();
+
+    System.out.printf("%nHello" + "%s", response);
     System.out.printf("%nGoodbye");
     System.out.printf("%nI'm Back!");
     System.out.printf("%nHope!");
