1df3dc13e05e9c1d679ed853b42bb76639b40d81
BlaineBeltran <55524257+ComputerEng21@users.noreply.github.com>
2020-06-05 15:48:11 -0500

    Added going away statement

--- test.java
+++ test.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Test {
   public static void main(String Args[]) {
 
-    public String response;
+    String response = "";
 
     System.out.printf("%nWhat is your name?  ");
-    response = input.nextLine();
+    // response = input.nextLine();
 
     System.out.printf("%nHello" + "%s", response);
     System.out.printf("%nGoodbye");
     System.out.printf("%nI'm Back!");
     System.out.printf("%nHope!");
+    System.out.printf("%nIm going away");
   }// END main()
 }// END APPLICATION class Testing
